1e8e9e16131e3b9df5d81104513d6a1e9fc99bbc
vonknorring <waltervk07@gmail.com>
2026-03-02 13:29:45 +0100

    skapat en register sida som tar dig till en profil sida

---
+++ public/profile.php
@@ -0,0 +1,10 @@
+<?php include "../header.php"; ?>
+<?php
+
+
+if(isset($_POST["createuser"])){
+    echo "data finns, skicka till db";
+var_dump($_POST);
+
+};
+?>
--- public/register.php
+++ public/register.php
@@ -1,13 +1,13 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
+<?php include "../header.php"; ?>
+
 <body>
+    <form action="profile.php" method="post">
+    <input type="text" name="name" placeholder="Name">
+    <input type="password" name="password" placeholder="Password">
+    <input type="submit" name="createuser" value="Create">
     <?php
     
     ?>
+    </form>
 </body>
 </html>
